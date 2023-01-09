60a69a91c10687527c0e5b191a7b6c01b721df53
Eilgnaw <wzqwzq666@hotmail.com>
2023-01-09 18:36:08 +0800

    Initial Commit

---
+++ ZhiYin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ZhiYin
+//
+//  Created by 王小劣 on 2023/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZhiYin/ZhiYinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhiYinApp.swift
+//  ZhiYin
+//
+//  Created by 王小劣 on 2023/1/9.
+//
+
+import SwiftUI
+
+@main
+struct ZhiYinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
