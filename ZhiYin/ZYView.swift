55200b45559566957c1c2dff94dd2d4e4ed8cb75
xinbingnan <xinbingnan@xiaomi.com>
2023-01-15 13:17:14 +0800

    修复：暗色主题下看不清的问题

--- ZhiYin/ZYView.swift
+++ ZhiYin/ZYView.swift
@@ -12,22 +12,34 @@ struct ZYView: View {
     @State var image = 0
     @State var imageName = "sssss163"
     @State var flag = false
+    @Environment(\.colorScheme) var currentMode
     
     let imageNum = 17
     
     var body: some View {
         let timer = Timer.publish(every: TimeInterval((1 - cpuInfo.cuse) / 10), on: .main, in: .common).autoconnect()
         VStack {
-            Image(imageName)
-                .resizable()
-                .frame(width: 22, height: 22)
-                .onReceive(timer) { _ in
-                    if image == 16 || image == 0 {
-                        flag.toggle()
-                    }
-                    image = flag ? (image + 1) % imageNum : (image - 1) % imageNum
-                    imageName = "sssss\(image + 163)"
+            VStack {
+                if currentMode == .dark {
+                    Image(imageName).resizable().colorInvert()
+                } else {
+                    Image(imageName).resizable()
                 }
+            }
+            .frame(width: 22, height: 22)
+            .onReceive(timer) { _ in
+                if image == 16 || image == 0 {
+                    flag.toggle()
+                }
+                image = flag ? (image + 1) % imageNum : (image - 1) % imageNum
+                imageName = "sssss\(image + 163)"
+            }
         }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZYView()
     }
 }
